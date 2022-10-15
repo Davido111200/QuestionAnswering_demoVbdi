58fc30cb29ff194e96f5db81bb0ed2ab24506098
Đỗ Văn Đại (VNCDLL-CTÐTKSAI-HVCTÐTKSAI) <daidv8@vingroup.net>
2022-10-15 14:37:40 +0700

    streamlit added

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,1 +1,73 @@
+import streamlit as st
+import sys
+sys.path.append(r'..\..\AnswerExtraction_demoVbdi')
+from passage_retrieval.googlesearch import GoogleSearch
+from passage_retrieval.passage_retrieval import PassageRetrieval
+import operator
+  
+
+from mrc.AnswerExtractionScoring import AnswerExtractor
+
+st.set_page_config(layout="wide", page_icon="🖱️", page_title="Search engine tiny")
+st.title('Question Answering')
+
+def get_answer(question):
+    gg = GoogleSearch()
+    retriever = PassageRetrieval("dpr")
+    passages_ = retriever.search(question, gg.search(question))
+    passages = [passages_[i][1] for i in range(len(passages_))]
+    answer_extractor = AnswerExtractor()
+    answers = answer_extractor.extract(question, passages)
+    return answers
+
+def get_k_top_answer(answers, k):
+    return answers.get("answer")[0:k]
+
+
+def app():
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # st.checkbox("Hỏi thật hay đùa?", key="disabled")
+        type_of_search = st.radio(
+            "What type of search algorithm do you want to choose?",
+            ('bm25', 'dpr'))
+
+        if type_of_search == 'bm25':
+            st.write('You selected bm25.')
+        elif type_of_search == 'dpr':
+            st.write('You selected dpr')
+        
+        num_answers = st.number_input(
+            "How many answers do you want to receive?",
+            min_value=1,
+            max_value=5,
+            format="%d"
+        )
+
+        question = st.text_input(
+            "Please input your question:",
+            "E.g: Who is Fidel Castro?",
+            key="placeholder",
+        )
+        
+        done = st.button(
+            "Search"
+        )
+    if done:
+
+        with col2:
+            with st.spinner(text="🤖 Finding Answers..."):
+                answers = get_answer(question)
+                first_k_answers = get_k_top_answer(answers, num_answers)
+            st.write(first_k_answers)
+            st.balloons()
+
+
+if __name__ == "__main__":
+    app()
 
